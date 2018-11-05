6399e83b87f6a1d49e34684b446b57c18ce2330e
kategomezny <35971954+kategomezny@users.noreply.github.com>
2018-11-05 15:30:22 -0500

    Update query_pets.py

--- query_pets.py
+++ query_pets.py
@@ -1,63 +1,31 @@
 #!/usr/bin/python
-
 # -*- coding: utf-8 -*-
-
 """Connects and query the data"""
 
-
-
 import sqlite3 as lite
-
-
 
 con = lite.connect('pets.db')
 
 
-
-
-
 while True:
-
         person_ID= raw_input("Please enter your ID number or enter -1 to exit:")
-
         if person_ID == '-1':
-
             print 'Exiting Database Query.'
-
             raise SystemExit
 
-
-
     
-
         cur = con.cursor() 
-
         
-
         
-
         cur.execute( "SELECT person.first_name || ' ' || person.last_name|| ', '|| person.age\
-
                     ||' years old and owns ' FROM person WHERE person.id=" + person_ID)
-
         results =  cur.fetchone()
-
         print results
-
         
-
         
-
         
-
         cur.execute( "SELECT  pet.name || ', a ' || pet.breed || ', that is ' || pet.age || ' years old'\
-
                 FROM person INNER JOIN person_pet ON (person.id = person_pet.person_id) \
-
                 INNER JOIN pet ON (person_pet.pet_id = pet.id) WHERE person.id=" + person_ID)
-
         results =  cur.fetchall()
-
         print results
-
-
