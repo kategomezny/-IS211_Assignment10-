cc3e2788dd816a3e3d2709bc4d2b5e9c96ab5ef3
kategomezny <35971954+kategomezny@users.noreply.github.com>
2018-11-05 15:27:54 -0500

    Update music.sql

--- music.sql
+++ music.sql
@@ -1,45 +1,23 @@
 CREATE TABLE Artist 
-
 (
-
     Artist_Id int PRIMARY KEY,
-
     Artist_Name  varchar NOT NULL
-
 );
-
    
-
         
-
 CREATE TABLE Albums
-
 (
-
     album_id int PRIMARY KEY,
-
     artist_id int NOT NULL REFERENCES Artist(artist_id),
-
     album_name varchar NOT NULL
-
 );
-
     
 
-
-
 CREATE TABLE Songs
-
 (
-
     song_id int PRIMARY KEY,
-
     song_name varchar NOT NULL,
-
     album_id int NOT NULL REFERENCES Albums(album_id),
-
     track_number int, 
-
     Duration int 
-
-    );
+);
